3b54572aa62601afd2f1bf21240c7508f853f524
John Lobdell <john.lobdell@gmail.com>
2019-06-24 16:21:54 -0600

    update check-ssh-fingerprint.sh to check against debian-ssh repo (https://github.com/g0tmi1k/debian-ssh)

--- check-ssh-fingerprint.sh
+++ check-ssh-fingerprint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Use this in conjunction with SSH Bad Keys:
-# https://github.com/rapid7/ssh-badkeys
-# The repo doesn't include fingerprints (which this checker uses).
-# To generate the fingerprints, use generate-fingerprints.sh
-# Change the file grep searches to the generated fingerprints file.
-
 if [[ $# -lt 1 ]]; then
 	echo "Checks SSH keys against known bad keys"
 	echo "Usage: $0 KEY_1 [... KEY_N]"
@@ -15,4 +9,8 @@ fi
 for KEY in "$@"
 do
 	grep "$KEY" ~/share/ssh-badkeys/fingerprints.txt
+	matches=$(ls -1R ~/share/debian-ssh/ | grep `echo $KEY | sed "s/://g"`)
+	for m in $matches; do
+		find ~/share/debian-ssh/ -name $m
+	done
 done
